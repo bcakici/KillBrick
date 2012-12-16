7ff6f1e0a4f6f22002696aa412e90d74b05b2028
Berkay Cakici <cbrky@in.com>
2012-12-16 14:17:41 +0200

    BallManager

--- Logic/BallManager.java
+++ Logic/BallManager.java
@@ -1,13 +1,29 @@
-import Data.Ball;
+package Logic;
+import Data.*;
+
+import java.util.ArrayList;
 /* it controls ball objects GameEngine*/
 public class BallManager {
 	private ArrayList<Ball> balls;
 	// before the game start this method is called to attach the first ball to pedal
-	public void attachFirstBallTo( Pedal p) {}
+	public void attachFirstBallTo( Pedal p) {
+		Ball firstBall = balls.get( 0);
+		firstBall.attachTo( p);
+	}
 	// returns all balls that are active
-	public ArrayList<Ball> getBalls() {	}
+	public ArrayList<Ball> getBalls() {
+		return balls;
+	}
 	// while the ball is moving this method updates the ball position.
-	public void redrawBalls() {}
+	public void redrawBalls() {
+		for( Ball b: balls){
+			b.redraw();
+		}
+	}
 	// unattach the first ball form pedal, and start moving it
-	public void unattachFirstBall() {}
+	public void unattachFirstBall() {
+
+		Ball firstBall = balls.get( 0);
+		firstBall.free();
+	}
 }
