b53f38d5f5f5bcbd9b2606c215f8e5e4a05fa67f
Dilan Yüksel <dilanyuksel@windowslive.com>
2012-12-16 15:13:39 +0200

    hıghscore list-add score

--- Data/HighScoreList.java
+++ Data/HighScoreList.java
@@ -9,19 +9,35 @@ public class HighScoreList {
 	//takes the users name and high score and writes to arrays.
 	public void addScore(int score, string name)
 	{
-
+	int place;
+        if(score > highScores.get(9))
+        {
+            for(int i=0 ; i< 10 ; i++)
+            {
+                if ( highScores.get(i) < score )
+                {
+                    place = i;
+                    highScores.add(i , score);
+                    i++;
+                    break;
+                }
+            }
+            names.add(place , name );
+            highScores.remove(10);
+            names.remove(10);
+        }
 	}
 
 	// get the highscores from array.
 	public ArrayList<Integer> getHighScores()
 	{
-
+        return highScores;
 	}
 
 	// get the highscores name from array.
 	public ArrayList<String> getHighScoreNames()
 	{
-
+        return names;
 	}
 
 }
