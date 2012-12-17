ac2a99bca4068dc0144cb3425d8b0c69fccee641
Berkay Cakici <cbrky@in.com>
2012-12-17 22:27:36 +0200

    Main method

--- KillBrick.java
+++ KillBrick.java
@@ -1,21 +1,10 @@
 import View.*;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author user
- */
 public class KillBrick {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	System.out.println("Running KillBrick");
+    	ViewController v = ViewController.getInstance();
+    	v.showMainMenu();
     }
 }
