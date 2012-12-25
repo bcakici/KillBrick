b8ca371a17719f390404d7e533b7483a92ac6798
Dilan Yüksel <dilanyuksel@windowslive.com>
2012-12-25 11:21:17 +0200

    Niceeee

--- Data/Point.java
+++ Data/Point.java
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-package Data;
-
 import Logic.GameObject;
-// it keeps cordinate of point x,y in any position.
-public class Point {
-
-	private int x;
-	private int y;
-
-	// it gets x cordinate
-	public int getX()
-	{
-        return x;
-	}
-
-	// it sets x cordinate.
-	public void setX(int x)
-	{
-        this.x = x;
-	}
-
-	//it gets y cordinate.
-	public int getY()
-	{
-        return y;
-	}
-
-	//it sets y cordinate.
-	public void setY(int y)
-	{
-        this.y = y;
-	}
-}
-=======
-package Data;
-
 // it keeps cordinate of point x,y in any position.
 public class Point {
 	private int x;
