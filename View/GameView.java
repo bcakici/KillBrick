adbec9618d5a7cb994957f02d51466ec5e9bc292
utekau <utekau@gmail.com>
2012-12-25 17:22:14 +0200

    Update View/GameView.java

--- View/GameView.java
+++ View/GameView.java
@@ -1,38 +1,35 @@
 //-	keeps the GameObject
 package View;
 import java.awt.*;
-
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import Data.NormalBrick;
-
 public class GameView extends JPanel{
-	//keeps the option of single or multi player game.
-	//creates GameEngine object
-        Logic.GameEngine gameEngine;
-	public void createGameEngine(boolean isMultiplayer, boolean sound) {
-            
-            if (isMultiplayer == true) {
-                gameEngine = new Logic.GameEngine(this, true);
-            }
-            else
-                gameEngine = new Logic.GameEngine(this, false);
+    //keeps the option of single or multi player game.
+    //creates GameEngine object
+    public GameView(){
+        
+    }
+    
+    Logic.GameEngine gameEngine;
+    public void createGameEngine(boolean isMultiplayer, boolean sound) {
+
+        if (isMultiplayer == true) {
+            gameEngine = new Logic.GameEngine(this,true);
+        }
+        else{
+            gameEngine = new Logic.GameEngine(this, false);
             startGame();
         }
+    }
+
+    // exits game.
+    public void exitGame() {
+        ViewController.getInstance().hideAllPanels();
+        ViewController.getInstance().setDefaultCloseOperation(ViewController.getInstance().EXIT_ON_CLOSE);
+    }
+    // stars the game.
+    public void startGame() {
+        gameEngine.createLevel(1);
+    }
         
-	// exits game.
-	public void exitGame() {
-            ViewController.getInstance().hideAllPanels();
-            ViewController.getInstance().setDefaultCloseOperation(ViewController.getInstance().EXIT_ON_CLOSE);
-        }
-	// stars the game.
-	public void startGame() {
-            gameEngine.createLevel(1);
-            gameEngine.redrawObjects();
-        }
-        
-        public void redraw(){
-            gameEngine.redrawObjects();
-        }
 }
