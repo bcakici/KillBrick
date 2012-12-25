3f1e6652b42dc802c74b081410cd387b163963b1
Dilan Yüksel <dilanyuksel@windowslive.com>
2012-12-25 12:35:29 +0200

    ball

--- Data/Ball.java
+++ Data/Ball.java
@@ -1,8 +1,12 @@
 //this class corresponds to ball in game
 public class Ball extends GameObject {
 	public BallManager;
+
 	// calculate the reflect ball form
-	public void reflectFrom(Point Collision) {}
+	public void reflectFrom(Point Collision)
+	{
+
+	}
 	// free the ball from pedal.
 	public void free() {}
 	// attach to ball to pedal.
@@ -11,26 +15,5 @@ public class Ball extends GameObject {
 	*  if it is a ball draws ball, if it is brick draws brick and so on */
 	public void redraw() {}
 
-
-     //Singleton instance
-    private static Ball ball = null;
-
-    //private constructor
-    private Ball(int x, int y){
-
-        this.x = x;
-        this.y = y;
-        dx = 0;
-        dy = 0;
-
-    }
-
-    //instantiates instance if there isn't already one
-    public static Ball getBall(int x, int y){
-        if(ball == null){
-            ball = new Ball(x, y);
-        }
-        return ball;
-    }
 }
 
